4fb241d5d20ea1396a727067244e28671281ea83
lillianiwaniszyn <lillian.iwaniszyn@gmail.com>
2017-10-17 21:37:48 -0600

    Add files via upload

--- Inspector.java
+++ Inspector.java
@@ -1,12 +1,21 @@
 package a2;
 
+import java.util.Arrays;
+
 public class Inspector {
 	public void inspect(Object obj, boolean recursive) {
 		//name of declaring class
+		Class aClass = obj.getClass();
 		String declaringClass = obj.getClass().getSimpleName();
 		System.out.print("The name of the declaring class is: ");
 		System.out.println(declaringClass);
-		
+		//name of immediate superclass
+		String immediateSuperClass = obj.getClass().getSuperclass().getSimpleName();
+		System.out.print("The name of the immediate superclass is: ");
+		System.out.println(immediateSuperClass);
+		//name of interfaces implemented
+		Class[] interfaces = aClass.getInterfaces();
+		System.out.println("Interfaces = " + Arrays.asList(interfaces));
 	}
 
 }
--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package a2;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Inspector inspect = new Inspector(); 
 		ClassA a = new ClassA();
 		inspect.inspect(a, true);
