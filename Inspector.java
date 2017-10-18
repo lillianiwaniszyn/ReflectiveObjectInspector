d551f06e4ba2abf20ffc4468dec0a6334fbae34f
lillianiwaniszyn <lillian.iwaniszyn@gmail.com>
2017-10-17 18:59:33 -0600

    Added Class A to test getting declaring class

--- Inspector.java
+++ Inspector.java
@@ -2,6 +2,10 @@ package a2;
 
 public class Inspector {
 	public void inspect(Object obj, boolean recursive) {
+		//name of declaring class
+		String declaringClass = obj.getClass().getSimpleName();
+		System.out.print("The name of the declaring class is: ");
+		System.out.println(declaringClass);
 		
 	}
 
---
+++ Main.java
@@ -0,0 +1,12 @@
+package a2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Inspector inspect = new Inspector(); 
+		ClassA a = new ClassA();
+		inspect.inspect(a, true);
+
+	}
+
+}
