e8977cdfd3a07ee855794169cfcc87ec68bdcce3
lillianiwaniszyn <lillian.iwaniszyn@gmail.com>
2017-10-17 22:04:10 -0600

    Added get exceptions and methods for each class

--- Inspector.java
+++ Inspector.java
@@ -1,11 +1,12 @@
 package a2;
 
+import java.lang.reflect.Method;
 import java.util.Arrays;
 
 public class Inspector {
 	public void inspect(Object obj, boolean recursive) {
 		//name of declaring class
-		Class aClass = obj.getClass();
+		Class classObj = obj.getClass();
 		String declaringClass = obj.getClass().getSimpleName();
 		System.out.print("The name of the declaring class is: ");
 		System.out.println(declaringClass);
@@ -14,8 +15,22 @@ public class Inspector {
 		System.out.print("The name of the immediate superclass is: ");
 		System.out.println(immediateSuperClass);
 		//name of interfaces implemented
-		Class[] interfaces = aClass.getInterfaces();
+		Class[] interfaces = classObj.getInterfaces();
 		System.out.println("Interfaces = " + Arrays.asList(interfaces));
+		//name of methods implemented
+		Method[] methods = classObj.getMethods();
+		for (Method i : methods) {
+			System.out.println();
+			Class [] exception = i.getExceptionTypes();		
+			System.out.print("Method: ");
+			System.out.print(i.getName());
+			for (Class j : exception) {
+				System.out.print(" Exception Name: ");
+				System.out.print(j.getName());
+			}
+
+		}
+		
 	}
 
 }
